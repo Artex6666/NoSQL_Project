4036fbbcbe1dd02af287b2e647238d4b9c4abdfd
ZARIC Slavko <slavkozaricz14@gmail.com>
2024-12-19 16:24:05 +0100

    Ajout de vue.js et vue-router

--- frontend/views/Home.vue
+++ frontend/views/Home.vue
@@ -1,1 +1,18 @@
+<template>
 
+  </div>
+</template>
+
+<script>
+import Auth from "./components/Auth.vue";
+
+export default {
+  components: {
+    Auth,
+  },
+};
+</script>
+
+<style>
+
+</style>
